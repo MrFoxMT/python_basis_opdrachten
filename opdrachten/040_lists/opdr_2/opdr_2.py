fbd8584d2e6191a8fc56dea27ad04ca4996f32ee
MrFoxMT <97106296@st.deltion.nl>
2023-11-30 15:17:14 +0100

    Versie 2

--- opdrachten/040_lists/opdr_2/opdr_2.py
+++ opdrachten/040_lists/opdr_2/opdr_2.py
@@ -13,3 +13,14 @@ rivieren = list(rivier_info.keys())
 # rivieren is nu een list met alleen de riviernamen: ['rijn', 'maas', 'nijl']
 
 # Hier jouw code.....
+
+print("De rivier de", rivieren[0].capitalize(), "stroomt onder andere door", rivier_info["rijn"][1].capitalize())
+
+print("De rivier de", rivieren[1].capitalize(), "stroomt onder andere door", rivier_info["maas"][0].capitalize())
+
+print("De rivier de", rivieren[2].capitalize(), "stroomt onder andere door", rivier_info["nijl"][1].capitalize())
+
+
+
+
+
