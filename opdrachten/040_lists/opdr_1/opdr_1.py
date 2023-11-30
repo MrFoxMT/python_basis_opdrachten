45a0ef662fcafc3da84c6f72147b8d4210a53a9a
MrFoxMT <97106296@st.deltion.nl>
2023-11-30 14:28:27 +0100

    Versie 2

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,18 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+
+dict_1 = "dict_1", 0, "Jaap", "Klinker"
+dict_2 = "dict_2", 1, "Don", "Kaaklijn"
+dict_3 = "dict_3", 2, "Joost", "Bouwhof"
+dict_4 = "dict_4", 3, "Micheal", "Johnson"
+
+mylist = [dict_1, dict_2, dict_3, dict_4]
+
+
+
+
+print(mylist[1][2], mylist[1][3])
+
+
+
