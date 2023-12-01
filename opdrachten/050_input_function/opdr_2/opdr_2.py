133963a90312bb945e5e6e5e7854ef7b5fbd92d3
MrFoxMT <97106296@st.deltion.nl>
2023-12-01 11:19:59 +0100

    Versie 3

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -9,3 +9,16 @@ gasten = ["Milan", "Paul", "Kees", "Marie", "Hilda"]
 del gasten[3]
 gasten.insert(3, 'George')
 print(gasten)
+
+
+
+stad_land1 = input("Land of stad 1 ")
+stad_land2 = input("Land of stad 2 ")
+stad_land3 = input("Land of stad 3 ")
+stad_land4 = input("Land of stad 4 ")
+stad_land5 = input("Land of stad 5 ")
+
+imputs = sorted([stad_land1, stad_land2, stad_land3, stad_land4, stad_land5])
+sort = sorted(imputs, reverse=True)
+print(sort)
+
--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -4,3 +4,32 @@
 
 # Hier komt je code...
 
+def user_input():
+    while True:
+        print("Typ 5 steden, of landen naar keuze.")
+        stad_land1 = input("Land of stad 1 ")
+        stad_land2 = input("Land of stad 2 ")
+        stad_land3 = input("Land of stad 3 ")
+        stad_land4 = input("Land of stad 4 ")
+        stad_land5 = input("Land of stad 5 ")
+        inputs = sorted([stad_land1, stad_land2, stad_land3, stad_land4, stad_land5], reverse=True)
+
+        print(f"Sorted inputs: {inputs}")
+
+        confirm = input("Dit zijn uw ingevoerde steden of landen, wilt u doorgaan? Typ dan Y of N ")
+
+        if confirm == "Y":
+            print("We gaan door.")
+            break
+        elif confirm == "N":
+            print("We gaan terug.")
+            continue
+        else:
+            print("Please only use Y or N")
+
+print("Bedankt")
+
+user_input()
+
+
+
