9a331d69158733a853c616890303ed831a0f334a
MrFoxMT <97106296@st.deltion.nl>
2023-12-01 09:38:22 +0100

    Versie 2

--- opdrachten/050_input_function/opdr_2/opdr_2.py
+++ opdrachten/050_input_function/opdr_2/opdr_2.py
@@ -1,7 +1,11 @@
 # Opdracht 2 berekeningen
-# Naam student:
-# Groep:
+# Naam student: Milan van de Klippe
+# Groep: ITX1
 
 # Hier komt je code...
 
-gasten = ["Jij", ... ]
+gasten = ["Milan", "Paul", "Kees", "Marie", "Hilda"]
+
+del gasten[3]
+gasten.insert(3, 'George')
+print(gasten)
